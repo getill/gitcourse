b54c293a7c71f5d20c9bd4aaccdb8a1c316d053c
Boris STOCKER <boris.stocker@gmail.com>
2022-12-13 10:35:52 +0000

    Replace main.c

--- src/main.c
+++ src/main.c
@@ -26,12 +26,21 @@ int main(int argc, char *argv[]){
 		r = _div(atof(a), atof(b));
 		printf("%lf",r);
 	}
-	else if (strcmp(op, "car") == 0) {
+	else {printf("Erreur de parametres");}
+	}
+	
+	if (argc == 3)
+	{
+	char* op = argv[1];
+	char* a = argv[2];
+	double r = 0;
+	if (strcmp(op, "car") == 0) {
 		r = _car(atof(a));
 		printf("%lf",r);
 	}
 	else {printf("Erreur de parametres");}
 	}
+	
 	else {printf("Erreur de parametres");}
 	return 0;
 }
